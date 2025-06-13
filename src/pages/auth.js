b2005c9eef548a63a6e1c3adaa0bc77b17bdf809
timerdar <timergazin24@mail.ru>
2025-06-13 11:31:57 +0500

    #2 - add api methods call for auth and sign-up pages

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -1,18 +1,53 @@
 export default function Auth() {
 
     setTimeout(() => {
-        document.getElementById("signin").addEventListener('click', () => {
-            location.hash = '#/menu'
+        document.getElementById("signin").addEventListener('click', async (e) => {
+            e.preventDefault();
+            const passwordHash = await hashPassword(document.getElementById("password").value);
+            const login = document.getElementById("login").value;
+
+            const response = await fetch("http://localhost:8081/auth", {
+                method: 'POST',
+                mode: "cors",
+                body: JSON.stringify({
+                    login: login,
+                    passwordHash: passwordHash
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            if (response.status === 200){
+                localStorage.setItem("login", login);
+                localStorage.setItem("passwordHash", passwordHash);
+                localStorage.setItem("userId", response.json().id);
+                location.hash = '#/menu';
+            }else if (response.status === 401){
+                document.getElementById("error").innerText = "Неправильный логин или пароль";
+            }else{
+                document.getElementById("error").innerText = response.json().error;
+            }
+
         })
     }, 0)
 
     return `
         <h1>Авторизация</h1>
+        <div id="error" class="error"></div>
         <form id="auth-form">
-            <input type="text" name="login" placeholder="Логин" class="auth-form" required>
-            <input type="password" name="password" placeholder="Пароль" class="auth-form" required>
+            <input type="text" id="login" name="login" placeholder="Логин" class="auth-form" required>
+            <input type="password" id="password" name="password" placeholder="Пароль" class="auth-form" required>
             <button type="submit" id="signin">Войти</button>
         </form>
         <a href="#/sign-up">Зарегистрироваться</a>
     `;
+}
+
+async function hashPassword(password) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(password);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
 }
--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -1,19 +1,56 @@
-export default function SignUp(){
+export default function SignUp() {
 
     setTimeout(() => {
-        document.getElementById('signup').addEventListener('click', () => {
-            location.hash = '/'
+        document.getElementById('signup').addEventListener('click', async (e) => {
+            e.preventDefault();
+            const login = document.getElementById('login').value;
+            const password = document.getElementById('password').value;
+            const passwordConf = document.getElementById('password-confirm').value;
+
+            if (password !== passwordConf) {
+                document.getElementById('error').innerText = "Пароли должны совпадать";
+            }else{
+                const hash = await hashPassword(password);
+                const response = await fetch("http://localhost:8081/sign-up", {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        login: login,
+                        passwordHash: hash 
+                    }),
+                    mode: 'cors'
+                }
+                )
+                if (response.status === 200){
+                    location.hash = '#/'
+                }else{
+                    document.getElementById('error').innerText = response.json().error;
+                }
+            }
+
         })
     }, 0);
 
     return `
         <h1>Регистрация</h1>
+        <div id="error" class="error"></div> 
         <form>
-            <input type="text" id="login" class="signup-form"/>
-            <input type="text" id="password" class="signup-form"/>
-            <input type="text" id="password-confirm" class="signup-form"/>
+            <input type="text" id="login" placeholder="Логин" class="signup-form" requered/>
+            <input type="password" id="password" placeholder="Пароль" class="signup-form" requered/>
+            <input type="password" id="password-confirm" placeholder="Подтверждение пароля" class="signup-form" requered/>
             <button type="submit" id="signup">Зарегистрироваться</button>
         </form>
     `;
 
+}
+
+async function hashPassword(password) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(password);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
 }
